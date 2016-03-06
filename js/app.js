6f0db8af12c3149eda2daf481b4c1b170875bf6c
Alexanderous <alexander.cheung@me.com>
2016-03-05 22:40:04 -0800

    Final Edit
    Added commentary and formatted to nanodegree standards

--- js/app.js
+++ js/app.js
@@ -1,104 +1,105 @@
+// TODO: add rocks as obstacles randomly strewn throughout.
 
-
-// Enemies our player must avoid
+/**
+* @description Create the enemies
+* @constructor
+* @param {number} x - the horizontal location of the enemy
+* @param {number} y - the vertical location of the enemy
+*/
 var Enemy = function(x, y) {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
-
     this.x = x;
     this.y = y;
+
+    // The speed is randomized, and optimized for player difficulty.
     this.speed = Math.floor(Math.random() * (220 - 40) + 40);
+
+    // The image/sprite for the enemies; the app uses
+    // a helper to easily load images
     this.sprite = 'images/enemy-bug.png';
+}
 
-};
-
-
-
-// Update the enemy's position, required method for game
+// This method updates the enemy's position.
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
+    // The x value is calculated by multiplying any movement by the dt
+    // parameter, which will ensure the game runs at the same speed for
     // all computers.
-
     this.x += dt * this.speed;
     if(this.x > 505) {
         this.x = -70;
-    }
+    };
+}
 
-};
-
-// Draw the enemy on the screen, required method for game
+// A method to draw the enemy on the screen.
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+}
 
-
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
+/**
+* @description Create a player
+* @constructor
+* @param {number} x - the horizontal location of the player
+* @param {number} y - the vertical location of the player
+*/
 var Player = function(x, y) {
+    // The image/sprite for the player; the app uses
+    // a helper to easily load images
     this.sprite = 'images/char-boy.png';
 
     this.x = x;
     this.y = y;
+
+    // The points variable tracks the player's score and level. The app
+    // uses this variable to calculate how many rows, bugs, and gems to add.
+    // The other two variables help to display the score and record score
+    // to the user.
     this.points = 0;
-    this.pointsGrande = this.points;
+    this.pointsGrande = 0;
     this.highScore = 0;
+}
 
-};
-
+// This method updates the player's position.
 Player.prototype.update = function() {
-    //this.score = this.points +
-    document.querySelector("#wordbar").innerHTML = "<h1>SCORE: " + this.pointsGrande + "</h1>";
-    document.querySelector("#wordbar2").innerHTML = "<h2>HIGH SCORE: " + this.highScore + "</h2>";
+    // This updates the current and high score, below the canvas.
+    document.querySelector('#wordbar').innerHTML = '<h1>SCORE: '
+     + this.pointsGrande + '</h1>';
+    document.querySelector('#wordbar2').innerHTML = '<h2>HIGH SCORE: '
+     + this.highScore + '</h2>';
+}
 
-};
-
+// This method renders the player on screen.
 Player.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
+}
 
-
+/**
+* @description Create the gems
+* @constructor
+* @param {number} x - the horizontal location of the gems
+* @param {number} y - the vertical location of the gems
+*/
 var Gem = function(x, y) {
     this.x = x;
     this.y = y;
+
+    // The speed of the gems is in a similar range as that of the enemies.
     this.speed = Math.floor(Math.random() * (220 - 40) + 40);
-/*     var gemImages = [
-        'images/Gem Blue.png',
-        'images/Gem Green.png',
-        'images/Gem Orange.png'
-    ];
-    var blueImage = ""
-    var greenImage = "" */
-};
 
-
-Gem.prototype.render = function() {
+    // The image/sprite for the gems; the app uses
+    // a helper to easily load images
     this.sprite = 'images/GemOrangeSmall.png';
-    /* var gemImages = [
-            'images/Gem Blue.png',
-            'images/Gem Green.png',
-            'images/Gem Orange.png'
-        ],
-        colu; */
+}
 
-    //for (colu = 0; colu < 4; colu++) {
-        ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// This method renders the gems on the screen.
+Gem.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
 
-    //};
-};
-
+// This method updates the gem's position based on time.
 Gem.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+    // The gems move back and forth, and so when a gem arrives at a certain
+    // x value, their movement switches to the opposite direction.
 
-    //this.x += dt * this.speed;
     if (this.x > 445) {
         this.east = false;
     } else if (this.x < 15) {
@@ -106,65 +107,48 @@ Gem.prototype.update = function(dt) {
     };
 
     if (this.east === true) {
+    // The gems move up in x value when they move "east".
         this.x += dt * this.speed;
     } else {
+    // The gems move down in x value when they move west, or not "east".
         this.x -= dt * this.speed;
     };
+    // I wanted to implement the conditional ternary operator here, but
+    // I thought it would overcomplicate the commenting.
 
-
-
-    /*this.y += dt * this.speed;
-    if(this.y > 300 + (player.points * 83)) {
-        this.y = dt * this.speed;
+    // This chunk of code was added to fix the bug that
+    // occurs when the game window is not the active
+    // window on the browser, which causes the gems to fling
+    // far right or left (as no above updates are made). When the
+    // game window would become active again, the gems would
+    // slowly make their way back to the canvas. With the code
+    // below, if the gems are found far away from the canvas, the
+    // app teleports them back to the canvas.
+    if (this.x > 600 || this.x < -1500) {
+        this.x = -70;
     };
-    if(this.y < 0) {
-        this.y += dt * this.speed;
-    } */
+}
 
-};
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-/* var BlueGem = function (x, y) {
-    Gem.call(this, (x, y));
-    this.sprite = 'images/Gem Blue.png';
-};
-
-BlueGem.protoype = Object.create(Gem.prototype);
-BlueGem.prototype.constructor = BlueGem;
-BlueGem.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
-};
-*/
-
-//var OrangeGem
-
-//var GreenGem
-
-
-
-
-
-var allEnemies = [new Enemy(-70, 54), new Enemy(-70, 54), new Enemy(-70, 137),
-                    new Enemy(-70, 137), new Enemy(-70, 220), new Enemy(-70, 220)];
+// Objects are instatiated below.
+// All enemy objects are placed in an array called allEnemies.
+// All gem objects are created in an aray called allGems.
+// A single player is built with an object variable called player.
+var allEnemies = [
+    new Enemy(-70, 54),
+    new Enemy(-70, 54),
+    new Enemy(-70, 137),
+    new Enemy(-70, 137),
+    new Enemy(-70, 220),
+    new Enemy(-70, 220)
+];
 
 var allGems = [new Gem(-70, 119), new Gem(-70, 202), new Gem(-70, 285)];
-
-/* var allGems = [new Gem(25, -70), new Gem(126, -70), new Gem(227, -70),
-                new Gem(328, -70), new Gem(429, -70)]; */
-//var blueGem = new Gem(-2, -70);
-//var allOrangeGems = [new Gem(99, -70), new Gem(200, -70), new Gem(301, -70)];
-//var greenGem = new Gem(402,-70);
-
 
 var player = new Player (200, 386);
 
 
-
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+// This listens for key presses and sends the keys to the
+// Player.handleInput() method.
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
@@ -174,14 +158,19 @@ document.addEventListener('keyup', function(e) {
     };
 
     player.handleInput(allowedKeys[e.keyCode]);
-});
+}) // Does this count as a function that doesn't need an ending semicolon?
 
+// This sets the tile dimensions as variables for calculating row add-ons
+// and player movements.
 var tileWidth = 101;
 var tileHeight = 83;
 
+// This method takes in the key inputs from the user and moves the player
+// accordingly. It also outlines certain consequences to specific movements.
 Player.prototype.handleInput = function(key){
     switch(key) {
         case 'left':
+            // This blocks the player from traveling to the left of the canvas space.
             if(this.x === -2) {
                 this.x = -2;
             } else {
@@ -190,6 +179,7 @@ Player.prototype.handleInput = function(key){
         break;
 
         case 'right':
+            // This blocks the player from traveling to the right of the canvas space.
             if(this.x === 402) {
              this.x = 402;
             } else {
@@ -197,30 +187,39 @@ Player.prototype.handleInput = function(key){
             }
         break;
 
-// reset player to original position after reaching water
-
         case 'up':
+            // If user presses "up" while at this tile, then he has crossed victoriously
+            // to the water. All consequences listed below.
             if(this.y === 54) {
+                // The player returns to start point, which y changes as canvas grows.
                 this.y = 386 + ((player.points + 1) * tileHeight);
                 this.x = 200;
+
+                // The points variable tracks the player's score and level. The app
+                // uses this variable to calculate how many rows, bugs, and gems to add.
                 this.points++;
+
+                // The pointsGrande variable calculates the score, which is the points
+                //value multiplied by 100, making the game more game-like and fun.
                 this.pointsGrande = this.points * 100
+
+                // This if statement updates the high score record to reflect the highest
+                // score achieved by the user.
                 if (this.pointsGrande > this.highScore) {
                     this.highScore = this.pointsGrande;
                 }
-                // When the canvas expands downward, this makes
-                // the site scroll down to the bottom of the page
-                // for ease of view of the avatar (which returned
-                // to the start location).
-                $( "#scorenotice" ).fadeIn( "slow");
-                window.scrollTo(0,document.body.scrollHeight);
 
+                // This line automatically scrolls the browser window down to the bottom
+                // of the screen, for ease of view of the avatar that has returned to the
+                // start position in a vertically expanded canvas.
+                window.scrollTo(0,document.body.scrollHeight);
             } else {
                 this.y -= tileHeight;
             }
         break;
 
         case 'down':
+            // This blocks the player from traveling below the canvas space.
             if(this.y === 386 + ((player.points) * tileHeight)) {
                 this.y = 386 + ((player.points) * tileHeight);
             } else {
@@ -228,35 +227,65 @@ Player.prototype.handleInput = function(key){
             }
         break;
     };
-};
+}
+// I experimented with implementing the conditional ternary operator for the above if else
+// statements, but it made them very hard to read. I kept the original if else format.
 
 
+// This function checks whether the player has collided with an enemy, and
+// then outlines the consequences.
+function checkCollisions() {
+    allEnemies.forEach(function(enemy) {
+        // This line defines what constitutes as a "collision." The actual images
+        // of the enemy and player both have quite large transparent backgrounds,
+        // so I added and subtracted some values to ensure that the images looked
+        // like they touched/collided.
+        if(enemy.x + 75 > player.x && enemy.x - 30 <= player.x && enemy.y ===
+            player.y) {
 
+            // This launches a modal with a "Game Over" notification.
+            $(function() {
+                $( '#gameover' ).dialog({
+                    modal: true
+                });
+            });
 
-/* if(Player.x) */
+            // This is the content within the "Game Over" modal.
+            document.querySelector('#gameover').innerHTML =
+                '<h1>Game Over!</h1><img src="images/Star-cropped.png" alt="Star"' +
+                ' height="80" width="80"><h3>Your Score: ' + player.pointsGrande +
+                '</h3><p>High Score: ' + player.highScore + '</p>';
 
-/*console.log(ctx); */
+            // The code below resets the player to the starting point and the scores
+            // and additional stone rows back to zero.
+            player.x = 200;
+            player.points = 0;
+            player.pointsGrande = 0;
 
-/* var Person = function(name) {
-    this.name = name;
-};
+            // This line ensures that the player's start position is on the bottom-
+            // most row, which y value changes as more stone rows are added after
+            // every victorious crossing.
+            player.y = 386 + (player.points * 83);
+        };
+    });
+}
 
-Person.prototype.display = function(greeting)
-{
-    console.log(greeting + " " + this.name);
-};
+// This is the function to check if player has collected a gem, and outlines the
+// attendant consequences.
+function checkPickups() {
+    allGems.forEach(function(gem) {
+        // Similar to the enemy collisions, I wanted to ensure that the player looked
+        // like he/she actually bumped up against a gem before "pickup."
+        if(gem.x + 20 > player.x && gem.x - 50 < player.x && gem.y - 65 === player.y) {
+            // After a gem is collected, it disappears for a while. The gem teleports
+            // to off-canvas left, and gradually makes its way back to the canvas.
+            gem.x = -1400;
 
-var new_person = new Person("Poornima");
-new_person.display("Good evening") ;
-} */
-
-/* function draw() {
-    // request to execute this function at the next earliest convenience
-    requestAnimationFrame(draw);
-    processInput();
-    moveObjectsAndEnemies();
-    drawAllTheThings();
-}; */
-
-
-//document.querySelector("#wordbar").innerHTML = "SCORE: "// + player.score;​
+            // After a gem is collected, both bugs on that row disappear for a while.
+            // They teleport to off-canvas left, but appear back on canvas shortly
+            // before the gems reappear, to make the game slightly more challenging.
+            allEnemies[allGems.indexOf(gem) * 2].x = -900;
+            allEnemies[(allGems.indexOf(gem) * 2) + 1].x = -900;
+        };
+    });
+}
--- js/engine.js
+++ js/engine.js
@@ -1,10 +1,10 @@
 /* Engine.js
  * This file provides the game loop functionality (update entities and render),
  * draws the initial game board on the screen, and then calls the update and
- * render methods on your player and enemy objects (defined in your app.js).
+ * render methods on the player and enemy objects (defined in app.js).
  *
  * A game engine works by drawing the entire game screen over and over, kind of
- * like a flipbook you may have created as a kid. When your player moves across
+ * like a flipbook you may have created as a kid. When the player moves across
  * the screen, it may look like just that image/character is moving or being
  * drawn but that is not the case. What's really happening is the entire "scene"
  * is being drawn over and over, presenting the illusion of animation.
@@ -33,7 +33,7 @@ var Engine = (function(global) {
      * and handles properly calling the update and render methods.
      */
     function main() {
-        /* Get our time delta information which is required if your game
+        /* This gets our time delta information which is required if your game
          * requires smooth animation. Because everyone's computer processes
          * instructions at different speeds we need a constant value that
          * would be the same for everyone (regardless of how fast their
@@ -42,47 +42,43 @@ var Engine = (function(global) {
         var now = Date.now(),
             dt = (now - lastTime) / 1000.0;
 
-        /* Call our update/render functions, pass along the time delta to
+        /* This calls our update/render functions, pass along the time delta to
          * our update function since it may be used for smooth animation.
          */
         update(dt);
         render();
 
-        /* Set our lastTime variable which is used to determine the time delta
-         * for the next time this function is called.
+        /* This sets our lastTime variable which is used to determine the time
+         * delta for the next time this function is called.
          */
         lastTime = now;
 
-        /* Use the browser's requestAnimationFrame function to call this
+        /* This uses the browser's requestAnimationFrame function to call this
          * function again as soon as the browser is able to draw another frame.
          */
         win.requestAnimationFrame(main);
     }
 
-    /* This function does some initial setup that should only occur once,
+    /** This function does some initial setup that should only occur once,
      * particularly setting the lastTime variable that is required for the
      * game loop.
      */
     function init() {
-        reset();
         lastTime = Date.now();
         main();
     }
 
     /* This function is called by main (our game loop) and itself calls all
-     * of the functions which may need to update entity's data. Based on how
-     * you implement your collision detection (when two entities occupy the
-     * same space, for instance when your character should die), you may find
-     * the need to add an additional function call here. For now, we've left
-     * it commented out - you may or may not want to implement this
-     * functionality this way (you could just implement collision detection
-     * on the entities themselves within your app.js file).
+     * of the functions which may need to update the entity's data. Here,
+     * it checks for gem "pickups" and enemy collisions. It also updates
+     * the canvas height.
      */
     function update(dt) {
         updateEntities(dt);
         checkPickups();
         checkCollisions();
-
+        // The game adds a row every single time the player reaches the water.
+        // In order to accommodate this growth, the canvas height increases.
         canvas.height = 606 + (player.points * 83);
     }
 
@@ -94,61 +90,16 @@ var Engine = (function(global) {
      * render methods.
      */
     function updateEntities(dt) {
-
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
         });
-        //blueGem.update();
+
         allGems.forEach(function(gem) {
             gem.update(dt);
         });
+
         player.update();
     }
-
-    function checkCollisions() {
-        allEnemies.forEach(function(enemy) {
-            if(enemy.x + 75 > player.x && enemy.x - 30 <= player.x && enemy.y === player.y) {
-
-                $(function() {
-                    $( "#gameover" ).dialog({
-                        modal: true,
-                        /*buttons: {
-                            Ok: function() {
-                                $( this ).dialog( "close" );
-                            }
-                        } */
-                    });
-                });
-
-                document.querySelector("#gameover").innerHTML =
-                '<h1>Game Over!</h1><img src="images/Star-cropped.png" alt="Star" height="80" width="80"><h3>Your Score: '
-                + player.pointsGrande + "</h3><p>High Score: " + player.highScore + "</p>";
-                player.x = 200;
-
-                player.points = 0;
-                player.pointsGrande = 0;
-                //enemy.x = 1000;
-                //player.wealth =
-                player.y = 386 + (player.points * 83);
-
-         };
-        });
-
-    };
-
-    function checkPickups() {
-            allGems.forEach(function(gem) {
-            //if(gem.y + 50 > player.y && gem.y - 30 <= player.y && gem.x === player.x) {
-            if(gem.x + 20 > player.x && gem.x - 50 < player.x && gem.y - 65 === player.y) {
-                //player.points
-                gem.x = -1400;
-                allEnemies[allGems.indexOf(gem) * 2].x = -900;
-                allEnemies[(allGems.indexOf(gem) * 2) + 1].x = -900;
-            };
-
-        });
-    };
-
 
     /* This function initially draws the "game level", it will then call
      * the renderEntities function. Remember, this function is called every
@@ -168,39 +119,45 @@ var Engine = (function(global) {
                 'images/grass-block.png',   // Row 1 of 2 of grass
                 'images/grass-block.png'    // Row 2 of 2 of grass
             ],
+
+            // The player.points variable adds another row per every successful journey
+            // to the water.
             numRows = player.points + 6,
             numCols = 5,
             original, row, col;
 
-        // This adds an additional stone row and two bugs every time
-        // the player reaches the water. For some reason, this causes
-        // an endless number of enemies to be created, so I truncate
-        // the array in the next chunk of code.
+        /* This adds an additional stone row (along with two bugs and a gem) every time
+         * the player reaches the water. For some reason, this causes an endless number
+         * of enemies to be created, so I truncate the array in the next chunk of code.
+        */
         for (original = 0; original < player.points; original++) {
+            // This inserts a stone row in between the water row and the first stone row.
             rowImages.splice(2, 0, 'images/stone-block.png');
+            // These two lines of code add enemies and a gem to the bottom-most stone road.
             allEnemies.push(new Enemy(-70, 220 + (83 * (player.points))),
                 new Enemy(-70, 220 + (83 * (player.points))));
             allGems.push(new Gem(-70, 285 + (83 * (player.points))));
         };
 
-        // This truncates the array to the expected length, so that there are
-        // two bugs for every lane.
+        /* These two lines truncate the array to the expected length, so that there are
+         * two bugs and one gem for every lane.
+        */
         allEnemies.length = 6 + (player.points * 2);
         allGems.length = 3 + player.points;
 
 
-        /* Loop through the number of rows and columns we've defined above
-         * and, using the rowImages array, draw the correct image for that
-         * portion of the "grid"
+        /* This loops through the number of rows and columns defined above
+         * and, using the rowImages array, draws the correct image for that
+         * portion of the "grid".
          */
         for (row = 0; row < numRows; row++) {
             for (col = 0; col < numCols; col++) {
                 /* The drawImage function of the canvas' context element
                  * requires 3 parameters: the image to draw, the x coordinate
                  * to start drawing and the y coordinate to start drawing.
-                 * We're using our Resources helpers to refer to our images
-                 * so that we get the benefits of caching these images, since
-                 * we're using them over and over.
+                 * This uses our Resources helpers to refer to the images
+                 * so that the user gets the benefits of caching these images,
+                 * since the game displays them over and over.
                  */
                 ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
             }
@@ -211,13 +168,14 @@ var Engine = (function(global) {
 
     /* This function is called by the render function and is called on each game
      * tick. Its purpose is to then call the render functions you have defined
-     * on your enemy and player entities within app.js
+     * on your enemy and player entities within app.js.
      */
     function renderEntities() {
-        /* Loop through all of the objects within the allEnemies array and call
-         * the render function you have defined.
+        /* This loops through all of the objects within the allEnemies and allGems
+         * array and calls the render function.
          */
 
+        // allGems is rendered first, so that the gems slide below the bugs.
         allGems.forEach(function(gem) {
             gem.render();
         });
@@ -226,27 +184,12 @@ var Engine = (function(global) {
             enemy.render();
         });
 
-        //blueGem.render();
-        /* allOrangeGems.forEach(function(gem) {
-        gem.render();
-        }); */
-
-
-
         player.render();
     }
 
-    /* This function does nothing but it could have been a good place to
-     * handle game reset states - maybe a new game menu or a game over screen
-     * those sorts of things. It's only called once by the init() method.
-     */
-    function reset() {
-        // noop
-    }
-
-    /* Go ahead and load all of the images we know we're going to need to
-     * draw our game level. Then set init as the callback method, so that when
-     * all of these images are properly loaded our game will start.
+    /* This loads all of the images to draw the game level. It then sets init as
+     * the callback method, so that when all of these images are properly loaded
+     * the game will start.
      */
     Resources.load([
         'images/stone-block.png',
@@ -254,15 +197,15 @@ var Engine = (function(global) {
         'images/grass-block.png',
         'images/enemy-bug.png',
         'images/char-boy.png',
-        //'images/Gem Blue.png',
-        'images/GemOrangeSmall.png'//,
-        //'images/Gem Green.png'
+        // The original orange gem file was too large and distracting, so I created
+        // a new smaller one.
+        'images/GemOrangeSmall.png'
     ]);
     Resources.onReady(init);
 
-    /* Assign the canvas' context object to the global variable (the window
-     * object when run in a browser) so that developers can use it more easily
-     * from within their app.js files.
+    /* This assigns the canvas' context object to the global variable (the window
+     * object when run in a browser) so that it can be used more easily
+     * from within the app.js files.
      */
     global.ctx = ctx;
 })(this);
